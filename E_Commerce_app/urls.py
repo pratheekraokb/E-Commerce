82cc210f658c47def6e754ff1a99c6ff3571fdb9
Pratheek Rao K B <pratheekmulleria@gmail.com>
2023-08-24 19:02:19 +0530

    djangi backend setup

--- E_Commerce_app/urls.py
+++ E_Commerce_app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
+
 
 urlpatterns = [
     # Define your app's URLs here
@@ -8,3 +11,5 @@ urlpatterns = [
     
     # Add more URLs as needed
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
