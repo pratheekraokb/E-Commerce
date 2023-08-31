70f2709a8400f917fb0dad78aa4520504df69113
= <pratheekmulleria@gmail.com>
2023-08-31 17:42:24 +0530

    Django project Setup

--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -73,10 +73,20 @@ WSGI_APPLICATION = 'E_Commerce.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ECommerce',
+        'USER': 'root',
+        'PASSWORD': 'Pratheek@2003',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
