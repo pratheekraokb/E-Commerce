6965ef121929e162fce8e34d95c46d0df6da0da0
= <pratheekmulleria@gmail.com>
2023-12-12 21:25:31 +0530

    Allowed Host modified

--- E_Commerce/settings.py
+++ E_Commerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*_qm&2nsl7jii1_g$!u@h_)bnfu=qfuqe5y@l%hb50fpaaq8)c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*','.vercel.app']
 
 
 # Application definition
